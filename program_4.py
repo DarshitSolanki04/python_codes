0d2ef461783e0abbb9382b59348e076f6bfda1e6
Darshit Solanki <darshit.solanki04@gmail.com>
2022-09-06 11:37:30 +0530

    Update program_4.py

--- program_4.py
+++ program_4.py
@@ -4,15 +4,14 @@
 list1 = [10, 20, 25, 30, 35]
 list2 = [40, 45, 60, 75, 90]
 
-lst = []             # We are defining lst as an empty list.
+lst = []
 
-for i in list1:      # Remember, here i takes a value in list1 not an index. i.e. the first value of i will be 10 not 0.
-    if i % 2 != 0:   # i%2 will calculate the remainder of division (i/2) if it is not equal to 0 that means that we have an odd number.
-        lst.append(i)# Remember, append() adds value at the end of the list.
+for i in list1:  # Remember, here 'i' take 'values' from list1, it is not an index. i.e. the first value of i will be 10 not 0.
+    if i % 2 != 0:  # i % 2 will calculate the remainder of division (i/2), if it is not equal to 0 that means we have an odd number.
+        lst.append(i)  # Remember, 'append()' adds an element at the end of the list.
 
 for i in list2:
     if i % 2 == 0:
         lst.append(i)
 
 print('Resulting list:', lst)
-
