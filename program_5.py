7888b71e13aa17f29652e128ae11276057231648
Darshit Solanki <darshit.solanki04@gmail.com>
2022-09-06 11:43:59 +0530

    Update program_5.py

--- program_5.py
+++ program_5.py
@@ -5,6 +5,6 @@ n = int(input('Enter a number: '))
 print('Result:', end=' ')
 
 while n > 0:
-    r = n % 10       # n%10 will always output the last digit in an integer. i.e. 3241%10 = 1 , 459%10 = 9.
-    print(r,end='')  # end='' is added to avoid the program printing a new line. By default, end='\n' which prints a new line. 
-    n = n // 10      # n//10 will do the same as n/10 but the result will be an integer. i.e. 3241//10 = 324 instead of 324.1.
+    r = n % 10  # n % 10 outputs the last digit of an integer, i.e. 3241 % 10 = 1, 459 % 10 = 9 etc.
+    print(r, end='')  # end='' is added to avoid the function printing a new line. By default, end='\n' which prints a new line. 
+    n = n // 10  # n // 10 will output the floor (the closest lower integer) of the result, i.e. 3.4 // 1 = 3, 3.9 // 1 = 3, 3241 // 10 = 324, etc.
