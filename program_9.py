0892ee4b1367eeabaa46814ea0fd9d62446ec766
Darshit Solanki <darshit.solanki04@gmail.com>
2022-09-06 11:53:40 +0530

    Update program_9.py

--- program_9.py
+++ program_9.py
@@ -4,16 +4,18 @@
 x = 1
 
 
-def exponent(base, exp): # this function calculates the exponent
-    global x
+def exponent(base, exp):
+    '''
+    This function calculates the exponent of a number.
+    '''
+    global x  # Will add the comment later.
     y = exp
     while y > 0:
-        x *= base
-        y -= 1
+        x *= base  # Equivalent to x = x * base.
+        y -= 1  # Similarly, y = y - 1.
     return base, exp
 
 
-baSe, eXp = exponent(5, 4) # LOOK HERE FIRST. When looking at code that is not your own, looking at functions first can make you confused.
-# in the line above we are sending 2 values to exponent function 5 as base and 4 as exp, we are recieving the values of base and exp (that are returned by the function) in baSe and eXp.
+baSe, eXp = exponent(5, 4)  # LOOK HERE FIRST. When looking at code that is not your own, looking at functions first can make you confused.
+# In the line above we are passing 2 values to the exponent function, 5 as base and 4 as exp, we are recieving the values of base and exp (that are returned by the function) in baSe and eXp.
 print('\n', baSe, 'raised to the power', eXp, ':', x)
-
