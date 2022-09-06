9d38ef03494e1023637a000d773ea2a5505b545a
Darshit Solanki <darshit.solanki04@gmail.com>
2022-09-06 11:49:06 +0530

    Update program_8.py

--- program_8.py
+++ program_8.py
@@ -6,7 +6,9 @@ This program prints an upside down right-angled triangle.
 * *
 *
 """
-for i in range(5, 0, -1): #Here we dictate that the loop starts with 5 and ends with 1 in -1 increments.
+
+
+for i in range(5, 0, -1):  # Here we dictate that the loop starts from 5 and ends with 1 in -1 increments.
     for j in range(i):
         print('*', end=' ')
-    print('') #prints a new line as by default end = '\n'.
+    print('')  # Prints a new line as by default, end='\n'.
