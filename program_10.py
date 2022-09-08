48cbfeebd3013cc58a6759e266d952c2fde12b75
Darshit Solanki <darshit.solanki04@gmail.com>
2022-09-09 01:33:56 +0530

    Update

--- program_10.py
+++ program_10.py
@@ -1,6 +1,19 @@
+'''
+Prints a right angled triangle with increasing rows.
+e.g.:
+1
+1 2
+1 2 3
+1 2 3 4
+
+etc.
+'''
+
+
 n = int(input('Enter the no. of rows: '))
 
 for i in range(1, n+1):
     for j in range(1, i+1):
         print(j, end=' ')
     print()
+
