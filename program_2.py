5285af4c0e1e4f838b5334cbd91171f6c235c7f4
Darshit Solanki <darshit.solanki04@gmail.com>
2022-09-10 19:36:35 +0530

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -9,4 +9,3 @@ for curr_number in range(10):  # Initially both the numbers are same, i.e. zero.
     # print('Current Number', i, ' Previous Number', j, ' Sum:', i+j)
     print('Current Number %d Previous Number %d Sum: %d' % (curr_number, prev_number, curr_number+prev_number))
     prev_number = curr_number
-
