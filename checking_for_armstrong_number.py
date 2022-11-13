e88e924035f078f537f9cfd662bfce32939230f3
Darshit Solanki <darshit.solanki04@gmail.com>
2022-11-13 15:33:40 +0530

    Update

--- checking_for_armstrong_number.py
+++ checking_for_armstrong_number.py
@@ -1,30 +1,23 @@
 """
 Checks whether the input number is an armstrong number or not.
-
-Armstrong number is a number in which when each digit is raised to the power of the total number of digits in the original number and summed then it returns the same original number. e.g.: 8208 -> 8^4 + 2^4 + 0^4 + 8^4 = 8208.
 """
 
 
-n = int(input("Enter a number: "))
+n = input('Type a positive integer: ')
 
-x = n
-t = n
+num_of_digits = len(n)
+m = int(n)
 
-cnt = 0
-z = 0
+num = 0
 
-while x > 0:  # This loop finds the total no. of digits in the input number.
-    x = x // 10
-    cnt += 1
-
-
-while t > 0:  # This loop finds the sum of all the digits raised to cnt.
-    r = t % 10
-    z = z + r**cnt
-    t = t // 10
-
-
-if z == n:
-    print("The given no. is an armstrong number.")
+if m < 0:
+    print(m, 'is not an Armstrong number.')
 else:
-    print("The given no. is not an armstrong number.")
+    for i in range(num_of_digits):
+        num += (m % 10) ** num_of_digits
+        m = int(m / 10)
+    
+    if int(n) == num:
+        print(n, 'is an Armstrong number.')
+    else:
+        print(n, 'is not an Armstrong number.')
