b00be47d88f917a1b6310a4d87d7ba1cc0eccaf3
Darshit Solanki <darshit.solanki04@gmail.com>
2022-11-13 14:54:33 +0530

    Update

--- check_prime_number.py
+++ check_prime_number.py
@@ -1,19 +1,20 @@
 '''
-This is a straight forward code to check if a number is a prime number or not.
-
-Note: This code is not efficient for checking very big prime numbers.
+Python code to check prime numbers.
 '''
 
+import math
 
-n = int(input('Enter a number: '))
 
-if n > 1:  # All prime numbers are greater than one. Note: One is not a prime number.
-    for i in range(2, n):  # In python, you can use 'for-else' loop. The 'else' loop is executed only if the 'for' loop completes fully if it breaks in-between the 'else' loop is skipped.
+n = int(input('Type a positive integer: '))
+
+if (n < 0):
+    print('Negative numbers can not be prime. Please type a positive integer.')
+elif (0 <= n < 2):
+    print(n, 'is not a prime number.')
+else:
+    for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             print(n, 'is not a prime number.')
             break
     else:
         print(n, 'is a prime number.')
-else:
-    print(n, 'is not a prime number.')
-
