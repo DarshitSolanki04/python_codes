145552fee315632ea7952bb3ca96788a1bf1ca6e
Darshit Solanki <darshit.solanki04@gmail.com>
2022-11-13 15:54:08 +0530

    Update

--- counting.py
+++ counting.py
@@ -2,9 +2,8 @@
 Counts how many times a particular word/fragment has  appeared in a string.
 '''
 
-var = 'Emma is a good developer. Emma is a writer.'
+var = input('Type a sentence: ')  # e.g. Emma is a good developer. Emma is a writer.
+n = input('Type a word: ')  # Program is case-sensitive.
+cnt = var.count(n)
 
-n = var.count('Emma')
-
-print('Emma appeared', n, 'times.')
-
+print(f'{n} appeared {cnt} times.')
